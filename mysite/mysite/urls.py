d0acfd08be028f0a31982009c199474918f7a741
atkincaid74@gmail.com <atkincaid74@gmail.com>
2019-07-25 22:36:44 -0400

    redirect

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import redirect_view
+
 urlpatterns = [
+    path('', redirect_view),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
