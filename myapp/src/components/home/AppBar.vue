801f89f48a65039463f0300e04b7ef59457f0698
atkincaid74@gmail.com <atkincaid74@gmail.com>
2019-08-08 20:18:50 -0400

    store installed and tested = ok

--- myapp/src/components/home/AppBar.vue
+++ myapp/src/components/home/AppBar.vue
@@ -1,10 +1,35 @@
 <template>
+  <v-app-bar app>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Andrew Kincaid</v-toolbar-title>
+    <v-spacer></v-spacer>
 
+    <v-btn
+      icon
+    >
+    <v-icon>{{ svgIcon }}</v-icon>
+    </v-btn>
+    <v-menu>
+
+    </v-menu>
+  </v-app-bar>
 </template>
 
 <script>
+  import { mdiAccountCircle } from '@mdi/js';
+  import { mapState } from 'vuex';
+
   export default {
-      name: "AppBar"
+      name: "AppBar",
+      data: () => ({
+          svgIcon: mdiAccountCircle,
+
+      }),
+      computed: {
+        ...mapState({
+
+        })
+      }
   }
 </script>
 
---
+++ myapp/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Es6Promise from 'es6-promise';
+
+Es6Promise.polyfill();
+Vue.use(Vuex);
+
+export const state = {
+
+
+};
+
+export const mutations = {
+
+};
+
+export const actions = {
+
+};
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions
+})
