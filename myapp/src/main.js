dc849806c119c9881cd7bebdc6b781d7e7f54cbc
atkincaid74@gmail.com <atkincaid74@gmail.com>
2019-08-15 00:38:58 -0400

    production changes

--- myapp/src/main.js
+++ myapp/src/main.js
@@ -16,5 +16,5 @@ new Vue({
   render: h => h(App)
 }).$mount('#app');
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = '';
 axios.defaults.headers['Authorization'] = `JWT ${store.state.token}`;
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,14 +25,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andrew-kincaid.herokuapp.com', 'andrew-kincaid.com',
+                 'localhost']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'polls.apps.PollsConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
@@ -55,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -129,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
