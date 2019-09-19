b571449821697ce4f96a487f68e88fc3cd1efc94
atkincaid74@gmail.com <ATKincaid74@gmail.com>
2019-09-19 14:47:27 -0400

    changed view site link

--- pickem/admin.py
+++ pickem/admin.py
@@ -6,3 +6,6 @@ from pickem.models import Team, Game, SeasonPickem
 admin.site.register(Team)
 admin.site.register(Game)
 admin.site.register(SeasonPickem)
+
+# change view site link in admin page to actual site
+admin.site.site_url = 'http://www.andrew-kincaid.com/'
