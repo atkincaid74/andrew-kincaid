1dff51bd7a4fe981b79ced694c833b4acdc0e973
atkincaid74@gmail.com <ATKincaid74@gmail.com>
2021-06-16 16:01:35 -0400

    privilege cleanup

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import ValidEmail
+from .models import ValidEmail, EmailPrivilege, UserPrivilege, PrivilegeLookup
 
 admin.site.register(ValidEmail)
+admin.site.register(EmailPrivilege)
+admin.site.register(UserPrivilege)
+admin.site.register(PrivilegeLookup)
